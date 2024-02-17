33dfa077600a931f67d5d57816a428e10e1f95fd
James <jamesrdaza@gmail.com>
2024-02-17 13:47:14 -0800

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
                         style={{
                             fontFamily: "Jetbrains Mono",
                             fontSize: "20px",
-                            textAlign: "center"
+                            textAlign: "center",
                         }}
                     >
                         Software Engineering &#x2022; Programming &#x2022;
@@ -39,24 +39,24 @@ function App() {
                     <span className="anchor" id="landingAbout"></span>
                     <h1 id="about">About Me</h1>
                     <p style={{ lineHeight: "2rem" }}>
-                        Hi! and Welcome to my personal website and portfolio! 
-                        I'm James Daza an aspiring
-                        Software Engineer, student,
-                        and programming-enthusiast. I'm driven and 
-                        passionate about computer science and problem-sovling.
-                        One of my interests include
-                        web development. I have interests in both frontend and backend 
-                        technologies.
-                        I strive to continuously expand
-                        my skill set and learn various things from languages, to libraries, to frameworks. 
-                        A lot of this learning is in my spare time, I study and research to keep up with what's new and emerging,
-                        especially in web development.
-                        One of my current undertakings is that I am in the process of learning Rust.
+                        Welcome to my portfolio! I'm James Daza, an aspiring
+                        Software Engineer and recent Computer Science graduate.
+                        With a B.S. degree earned in December 2023, I'm deeply
+                        passionate about computer science and problem-solving.
+                        My interests span various areas, including web
+                        development and blockchain technology. I enjoy working
+                        on both frontend and backend. I am constantly seeking to
+                        expand my skill set and delve into new languages,
+                        libraries, and frameworks. A lot of my learning occurs
+                        in my spare time, where I actively study and research to
+                        stay in touch with the latest developments. Currently,
+                        I'm diving into the world of Rust to further my
+                        skillset.
                     </p>
                 </div>
                 <div
                     className="halfContent"
-                    style={{ 
+                    style={{
                         gridColumn: "7 / span 5",
                         display: "flex",
                         justifyContent: "center",
@@ -78,42 +78,15 @@ function App() {
                             name="Javascript"
                             url="./images/logos/javascript.png"
                         />
-                        <Skill
-                            name="CSS"
-                            url="./images/logos/css3.png"
-                        />
-                        <Skill
-                            name="HTML"
-                            url="./images/logos/html5.png"
-                        />
-                        <Skill
-                            name="C/C++"
-                            url="./images/logos/c.png"
-                        />
-                        <Skill
-                            name="Java"
-                            url="./images/logos/java.png"
-                        />
-                        <Skill
-                            name="Python"
-                            url="./images/logos/python.png"
-                        />
-                        <Skill
-                            name="Git"
-                            url="./images/logos/git.png"
-                        />
-                        <Skill
-                            name="Vim"
-                            url="./images/logos/vim.png"
-                        />
-                        <Skill
-                            name="React"
-                            url="./images/logos/react.png"
-                        />
-                        <Skill
-                            name="Svelte"
-                            url="./images/logos/svelte.png"
-                        />
+                        <Skill name="CSS" url="./images/logos/css3.png" />
+                        <Skill name="HTML" url="./images/logos/html5.png" />
+                        <Skill name="C/C++" url="./images/logos/c.png" />
+                        <Skill name="Java" url="./images/logos/java.png" />
+                        <Skill name="Python" url="./images/logos/python.png" />
+                        <Skill name="Git" url="./images/logos/git.png" />
+                        <Skill name="Vim" url="./images/logos/vim.png" />
+                        <Skill name="React" url="./images/logos/react.png" />
+                        <Skill name="Svelte" url="./images/logos/svelte.png" />
                     </div>
                 </div>
             </section>
@@ -122,8 +95,49 @@ function App() {
                     <span className="anchor" id="landingProjects"></span>
                     <h1 id="projects">Projects</h1>
                     <div className="projectContainer">
-                        <Project title="Bullet Journal" displayURL="./images/projects/BuJo.png" 
-                            modalDesc="I worked in a team with ten other students to create an online Bullet Journal while utilizing the agile process. We had three distinct sprints for our team and did daily standups, weekly team meetings, and retrospectives to gauge our efficacy during development. We had three main groups during development. We had designers who created lo-fi and hi-fi designs of the UI, front-end who created the elements of the website, and back-end, where I was, that would figure out how to store and query. We used indexedDB to store user data in local storage and passed our functions to the front-end team. After the initial setup, I transitioned into a more testing role setting up jest scripts to test our indexedDB database. Passing the test script was required for pull requests into our main git branch."     
+                        <Project
+                            title="Bullet Journal"
+                            displayURL="./images/projects/BuJo.png"
+                            modalDesc="I collaborated with a team of ten students to develop an online Bullet Journal, 
+                            employing agile methodologies throughout the project. We structured our work into three distinct 
+                            sprints, conducting daily stand-ups, weekly team meetings, and retrospectives to assess our progress 
+                            and efficiency. Our development process involved three main groups: designers who crafted both 
+                            low-fidelity and high-fidelity UI designs, front-end developers responsible for implementing 
+                            website elements, and back-end developers, including myself, tasked with data storage and 
+                            retrieval solutions. In my role on the back-end team, I focused on devising strategies for 
+                            data storage and querying. We utilized indexedDB for local storage of user data, seamlessly 
+                            integrating our functions with the front-end team's work. As the project progressed, I 
+                            transitioned to a testing role, implementing Jest scripts to ensure the reliability of our 
+                            indexedDB database. Successful completion of these tests was a prerequisite for merging pull 
+                            requests into our main Git branch."
+                            link=""
+                            github=""
+                        />
+                        <Project
+                            title="Fotodumper"
+                            displayURL="./images/projects/Fotodumper.png"
+                            modalDesc="I am currently developing a website that allows users to upload images to their user profiles. 
+                            The user interface is being crafted by me using Svelte. On the backend, I'm utilizing a Postgres database to store 
+                            user information. This database stores a link to an AWS S3 bucket housing the images, alongside user authentication 
+                            information provided through Google's authentication API."
+                            link="https://github.com/jamesrdaza/fotodumper"
+                            github="https://github.com/jamesrdaza/fotodumper-api"
+                        />
+                        <Project
+                            title="Smart Contract Monitor"
+                            displayURL="./images/projects/EthSmrt.png"
+                            modalDesc="I created a react app that monitors a smart contract on the Ethereum blockchain. It uses the Etherscan API and 
+                            Alchemy API to get contract data and monitor unconfirmed transactions on the blockchain. It then sends out an transaction 
+                            that the user and calls a function that the user desires."
+                            link="https://github.com/jamesrdaza/Ethereum-Smart-Contract-Monitor"
+                            github="https://github.com/jamesrdaza/Ethereum-Smart-Contract-Monitor"
+                        />
+                        <Project
+                            title="Vois"
+                            displayURL="./images/projects/Vois.png"
+                            modalDesc="I developed a proof of concept Social Media Dapp featuring user authentication. Users can authenticate by signing a message with their wallet, establishing their identity. Subsequently, the system creates a user profile stored in a Postgres database. Management of the database was facilitated by Prisma ORM, enabling schema creation and seamless data handling through its provided functions."
+                            link="https://github.com/jamesrdaza/Vois"
+                            github="https://github.com/jamesrdaza/Vois"
                         />
                     </div>
                 </div>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -6,28 +6,47 @@ interface ProjectProps {
     title: string;
     displayURL: string;
     modalDesc: string;
+    link: string;
+    github: string;
 }
-const Project = ({ title, displayURL, modalDesc }: ProjectProps) => {
+const Project = ({
+    title,
+    displayURL,
+    modalDesc,
+    link,
+    github,
+}: ProjectProps) => {
     const [modalIsActive, setModalIsActive] = useState(false);
-    
+
     function flipModalIsActive() {
         setModalIsActive(!modalIsActive);
     }
     return (
-    <>
-            <div className="project" style={{display: modalIsActive ? "none" : "block"}} onClick={flipModalIsActive}>
+        <>
+            <div
+                className="project"
+                style={{ display: modalIsActive ? "none" : "block" }}
+                onClick={flipModalIsActive}
+            >
                 <img src={displayURL} alt={title} />
                 <h3>{title}</h3>
             </div>
-            <div 
-                className="projectModal" 
-                style={{display: modalIsActive ? "flex" : "none", }}
+            <div
+                className="projectModal"
+                style={{ display: modalIsActive ? "flex" : "none" }}
             >
-                <GrClose size={22} id="CloseModal" onClick={flipModalIsActive}/>
+                <GrClose
+                    size={22}
+                    id="CloseModal"
+                    onClick={flipModalIsActive}
+                />
                 <img src={displayURL} alt={title} />
                 <p>{modalDesc}</p>
+                <div className="linkContainer">
+                    <a href={link}>Link</a>|<a href={github}>Github</a>
+                </div>
             </div>
-    </>
+        </>
     );
 };
 
